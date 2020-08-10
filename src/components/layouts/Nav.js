c3ed7263b2db5ca7e854868079126e138d2591f0
John <johnrubio93@gmail.com>
2020-08-11 08:39:50 +1000

    added nav links

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -1,5 +1,32 @@
+import Link from "next/link";
 import styles from "./Nav.module.scss";
 
 export default function Nav() {
-  return <nav className={styles.nav}>Nav</nav>;
+  return (
+    <nav className={styles.nav}>
+      <img src="/vercel.svg" alt="Vercel Logo" />
+      <ul>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects">
+            <a>Projects</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/leisure">
+            <a>Leisure</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
