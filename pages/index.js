d093378718741e8458556854a4daa167a5d619cd
John <johnrubio93@gmail.com>
2020-08-22 01:08:52 +1000

    added button

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,27 @@ export default function Home() {
   return (
     <Layout title="Welcome">
       <div className={styles.home}>
-        <div>
+        <div className={styles.welcomeWrapper}>
           <div>
-            <h2>I am a Web Developer.</h2>
-            <p>
-              I build things. I break things. I make it daily duty to do so.
-            </p>
+            <h2>I am a Web Developer</h2>
+            <p>I build things.</p>
+            <p>I break things.</p>
+            <p>I make it daily duty to do so.</p>
           </div>
           <div>
             <h3>Latest.</h3>
             <p>Link to latest update</p>
           </div>
         </div>
-
+        <div className={styles.switchButton}>
+          <button>Turn it on</button>
+        </div>
         <div className={styles.mascotWrapper}>
-          <img src="/logo.svg" alt="Logo" className={styles.mascot} />
+          <img src="/logo.svg" alt="Logo" />
         </div>
       </div>
     </Layout>
   );
 }
+
+//inspire, create, learn. solve, flip the switch
