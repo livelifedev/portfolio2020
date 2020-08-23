a2f3c4986906ee23bbf4d5efd2c536c198c2d5cb
John <johnrubio93@gmail.com>
2020-08-23 10:39:14 +1000

    fix overflow issue

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -8,11 +8,11 @@ export default function Portfolio() {
       <div className={styles.portfolio}>
         <div className={styles.projects}>
           <h2>Projects Showcase</h2>
+          <p>Highlights of my time studying web development -</p>
           <p>
-            Highlights of my time studying web development - some are from my
-            early days as a beginner and others are side projects I've done in
-            my free time, trying out new technology or exploring ideas in my
-            head.
+            Some are from my early days as a beginner and others are side
+            projects I've done to try out new technologies or explore ideas in
+            my head.
           </p>
           <PortfolioCarousel />
         </div>
